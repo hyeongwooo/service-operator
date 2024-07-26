bf0acd91c28437a28326d0b3a9f6755b81d3fdf8
주형우 <wnguddn777@juhyeong-uui-MacBookAir.local>
2024-07-26 22:31:46 +0900

    add logs

--- internal/controller/serviceoperator_controller.go
+++ internal/controller/serviceoperator_controller.go
@@ -1,25 +1,8 @@
-/*
-Copyright 2024.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package controller
 
 import (
 	"context"
 	"fmt"
-	"strings"
 
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -34,7 +17,6 @@ import (
 	operatorv1alpha1 "github.com/wnguddn777.com/service-operator/api/v1alpha1"
 )
 
-// ServiceOperatorReconciler reconciles a ServiceOperator object
 type ServiceOperatorReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
@@ -46,19 +28,15 @@ type ServiceOperatorReconciler struct {
 //+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
 
-// Reconcile reconciles a ServiceOperator object
 func (r *ServiceOperatorReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := log.FromContext(ctx)
 
-	// Retrieve the custom resource
 	customResource := &operatorv1alpha1.ServiceOperator{}
-	err := r.Client.Get(context.Background(), req.NamespacedName, customResource)
+	err := r.Client.Get(ctx, req.NamespacedName, customResource)
 	if err != nil {
 		if errors.IsNotFound(err) {
-			// Custom resource not found, delete associated resources
 			log.Info(fmt.Sprintf(`Custom resource for service "%s" does not exist, deleting associated resources`, req.Name))
 
-			// Delete resources
 			deployErr := r.Client.Delete(ctx, newDeployment(req.Name, req.Namespace, "n/a", nil, nil, corev1.ResourceRequirements{}))
 			if deployErr != nil && !errors.IsNotFound(deployErr) {
 				log.Error(deployErr, fmt.Sprintf(`Failed to delete deployment "%s"`, req.Name))
@@ -80,63 +58,116 @@ func (r *ServiceOperatorReconciler) Reconcile(ctx context.Context, req ctrl.Requ
 		}
 	}
 
-	// Create or update Deployment
-	err = r.Client.Create(ctx, newDeployment(req.Name, req.Namespace, customResource.Spec.Image, customResource.Spec.ContainerPort,
-		customResource.Spec.Replicas, customResource.Spec.Resources))
+	deployment := &appsv1.Deployment{}
+	deploymentNamespacedName := types.NamespacedName{
+		Name:      customResource.Name,
+		Namespace: customResource.Namespace,
+	}
+
+	err = r.Client.Get(ctx, deploymentNamespacedName, deployment)
 	if err != nil {
-		if errors.IsAlreadyExists(err) {
-			log.Info(fmt.Sprintf(`Deployment for service "%s" already exists`, customResource.Name))
-			deploymentNamespacedName := types.NamespacedName{
-				Name:      customResource.Name,
-				Namespace: customResource.Namespace,
-			}
-
-			deployment := appsv1.Deployment{}
-			r.Client.Get(ctx, deploymentNamespacedName, &deployment)
-
-			currentImage := deployment.Spec.Template.Spec.Containers[0].Image
-			desiredImage := customResource.Spec.Image
-
-			if currentImage != desiredImage {
-				log.Info(fmt.Sprintf(`Image has updated from "%s" to "%s"`, currentImage, desiredImage))
-
-				patch := client.StrategicMergeFrom(deployment.DeepCopy())
-				deployment.Spec.Template.Spec.Containers[0].Image = desiredImage
-				err := r.Client.Patch(ctx, &deployment, patch)
-				if err != nil {
-					log.Error(err, fmt.Sprintf(`Failed to update deployment "%s"`, customResource.Name))
-					return ctrl.Result{}, err
-				}
+		if errors.IsNotFound(err) {
+			log.Info(fmt.Sprintf(`Deployment for service "%s" does not exist, creating new one`, customResource.Name))
+			err = r.Client.Create(ctx, newDeployment(req.Name, req.Namespace, customResource.Spec.Image, customResource.Spec.ContainerPort, customResource.Spec.Replicas, customResource.Spec.Resources))
+			if err != nil {
+				log.Error(err, fmt.Sprintf(`Failed to create deployment "%s"`, customResource.Name))
+				return ctrl.Result{}, err
 			}
 		} else {
-			log.Error(err, fmt.Sprintf(`Failed to create deployment "%s"`, customResource.Name))
+			log.Error(err, fmt.Sprintf(`Failed to get deployment "%s"`, customResource.Name))
 			return ctrl.Result{}, err
+		}
+	} else {
+		// Check for changes and update the Deployment if necessary
+		updateDeployment := false
+
+		if deployment.Spec.Template.Spec.Containers[0].Image != customResource.Spec.Image {
+			log.Info(fmt.Sprintf(`Deployment image changed from "%s" to "%s"`, deployment.Spec.Template.Spec.Containers[0].Image, customResource.Spec.Image))
+			deployment.Spec.Template.Spec.Containers[0].Image = customResource.Spec.Image
+			updateDeployment = true
+		}
+
+		if *deployment.Spec.Replicas != *customResource.Spec.Replicas {
+			log.Info(fmt.Sprintf(`Deployment replicas changed from "%d" to "%d"`, *deployment.Spec.Replicas, *customResource.Spec.Replicas))
+			deployment.Spec.Replicas = customResource.Spec.Replicas
+			updateDeployment = true
+		}
+
+		if updateDeployment {
+			err = r.Client.Update(ctx, deployment)
+			if err != nil {
+				log.Error(err, fmt.Sprintf(`Failed to update deployment "%s"`, customResource.Name))
+				return ctrl.Result{}, err
+			}
 		}
 	}
 
-	// Create or update Service
-	err = r.Client.Create(ctx, newService(req.Name, req.Namespace, customResource.Spec.ServiceType, customResource.Spec.NodePort, customResource.Spec.Port))
+	// Determine the service type
+	serviceType := customResource.Spec.ServiceType
+	if serviceType == "" {
+		serviceType = corev1.ServiceTypeClusterIP
+	}
+
+	service := &corev1.Service{}
+	serviceNamespacedName := types.NamespacedName{
+		Name:      customResource.Name,
+		Namespace: customResource.Namespace,
+	}
+
+	err = r.Client.Get(ctx, serviceNamespacedName, service)
 	if err != nil {
-		if errors.IsInvalid(err) && strings.Contains(err.Error(), "provided port is already allocated") {
-			log.Info(fmt.Sprintf(`Service "%s" already exists`, customResource.Name))
-			// TODO: handle service updates gracefully
+		if errors.IsNotFound(err) {
+			log.Info(fmt.Sprintf(`Service for "%s" does not exist, creating new one`, customResource.Name))
+			err = r.Client.Create(ctx, newService(req.Name, req.Namespace, customResource.Spec.ServiceType,
+				customResource.Spec.NodePort, customResource.Spec.Port))
+			if err != nil {
+				log.Error(err, fmt.Sprintf(`Failed to create service "%s"`, customResource.Name))
+				return ctrl.Result{}, err
+			}
 		} else {
-			log.Error(err, fmt.Sprintf(`Failed to create service "%s"`, customResource.Name))
+			log.Error(err, fmt.Sprintf(`Failed to get service "%s"`, customResource.Name))
 			return ctrl.Result{}, err
+		}
+	} else {
+		// Check for changes and update the Service if necessary
+		updateService := false
+
+		if service.Spec.Type != customResource.Spec.ServiceType {
+			log.Info(fmt.Sprintf(`Service type changed from "%s" to "%s"`, service.Spec.Type, customResource.Spec.ServiceType))
+			service.Spec.Type = customResource.Spec.ServiceType
+			updateService = true
+		}
+
+		if len(service.Spec.Ports) > 0 && service.Spec.Ports[0].Port != *customResource.Spec.Port {
+			log.Info(fmt.Sprintf(`Service port changed from "%d" to "%d"`, service.Spec.Ports[0].Port, *customResource.Spec.Port))
+			service.Spec.Ports[0].Port = *customResource.Spec.Port
+			updateService = true
+		}
+
+		if service.Spec.Type == corev1.ServiceTypeNodePort && customResource.Spec.NodePort != nil && service.Spec.Ports[0].NodePort != *customResource.Spec.NodePort {
+			log.Info(fmt.Sprintf(`Service NodePort changed from "%d" to "%d"`, service.Spec.Ports[0].NodePort, *customResource.Spec.NodePort))
+			service.Spec.Ports[0].NodePort = *customResource.Spec.NodePort
+			updateService = true
+		}
+
+		if updateService {
+			err = r.Client.Update(ctx, service)
+			if err != nil {
+				log.Error(err, fmt.Sprintf(`Failed to update service "%s"`, customResource.Name))
+				return ctrl.Result{}, err
+			}
 		}
 	}
 
 	return ctrl.Result{}, nil
 }
 
-// SetupWithManager sets up the controller with the Manager.
 func (r *ServiceOperatorReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&operatorv1alpha1.ServiceOperator{}).
 		Complete(r)
 }
 
-// setResourceLabels sets labels for resources created by the operator
 func setResourceLabels(name string) map[string]string {
 	return map[string]string{
 		"serviceoperator": name,
@@ -144,7 +175,6 @@ func setResourceLabels(name string) map[string]string {
 	}
 }
 
-// newDeployment creates a new Deployment resource
 func newDeployment(name, namespace, image string, containerPort *int32, replicas *int32, resources corev1.ResourceRequirements) *appsv1.Deployment {
 	defaultReplicas := int32(2)
 	if replicas == nil {
@@ -184,20 +214,16 @@ func newDeployment(name, namespace, image string, containerPort *int32, replicas
 	}
 }
 
-// newService creates a new Service resource
 func newService(name, namespace string, serviceType corev1.ServiceType, nodePort, port *int32) *corev1.Service {
-	// Set default port if not specified
 	if port == nil {
 		defaultPort := int32(80)
 		port = &defaultPort
 	}
 
-	// Initialize the ServicePort
 	servicePort := corev1.ServicePort{
 		Port: *port,
 	}
 
-	// Initialize the Service object
 	service := &corev1.Service{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      name,
@@ -211,11 +237,8 @@ func newService(name, namespace string, serviceType corev1.ServiceType, nodePort
 		},
 	}
 
-	// Set NodePort if the ServiceType is NodePort and NodePort is specified
-	if serviceType == corev1.ServiceTypeNodePort {
-		if nodePort != nil {
-			service.Spec.Ports[0].NodePort = *nodePort
-		}
+	if serviceType == corev1.ServiceTypeNodePort && nodePort != nil {
+		service.Spec.Ports[0].NodePort = *nodePort
 	}
 
 	return service
